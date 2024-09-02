291b8bf940b997d8e8b7a81dd9ffdda456f69dd2
Karabo Lamola <lamola1@msn.com>
2024-09-02 08:58:57 +0200

    Added IGenericRepository.cs

--- HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
+++ HR.LeaveManagement.Application/Persistance/Contracts/IGenericRepository.cs
@@ -1,9 +1,14 @@
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Persistance.Contracts
 {
     public interface IGenericRepository<T> where T : class
     {
-        Task<T> Get(int id);
+        Task<T> GetAsync(int id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
     }
 }
