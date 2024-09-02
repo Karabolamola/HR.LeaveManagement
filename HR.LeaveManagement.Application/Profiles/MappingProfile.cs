0bf4174c45cbb23783f11e4a3c30ce4b4386965c
Karabo Lamola <lamola1@msn.com>
2024-09-02 09:34:11 +0200

    Changes to MappingProfile.cs

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -1,9 +1,16 @@
 using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Profiles
 {
     public class MappingProfile : Profile
     {
-        
+        public MappingProfile()
+        {
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+        }
     }
 }
