189e316c5cf573e656cea8fa1d40219367b2ce84
Karabo Lamola <lamola1@msn.com>
2024-09-02 08:46:49 +0200

    Added Domain classes and BaseDomainEntity.cs to inherit common fields from

---
+++ HR.LeaveManagement.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HR.LeaveManagement.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+    }
+}
