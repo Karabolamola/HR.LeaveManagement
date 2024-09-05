44947ac38c52ae7d7acb83ea3b1c9a40b7e54b7d
Karabo Lamola <lamola1@msn.com>
2024-09-05 16:32:39 +0200

    Changes

--- HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequests/Handlers/Commands/CreateLeaveRequestCommandHandler.cs
@@ -10,6 +10,8 @@ using HR.LeaveManagement.Application.Contracts.Persistence;
 using HR.LeaveManagement.Application.Responses;
 using HR.LeaveManagement.Domain;
 using MediatR;
+using HR.LeaveManagement.Application.Contracts.Infrastructure;
+using HR.LeaveManagement.Application.Models;
 
 namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Commands
 {
@@ -17,18 +19,20 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
     {
         private readonly ILeaveRequestRepository _leaveRequestRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
 
-        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper)
+        public CreateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, IMapper mapper, IEmailSender emailSender)
         {
             _leaveRequestRepository = leaveRequestRepository;
             _mapper = mapper;
+            _emailSender = emailSender;
         }
-        
-        public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand requestCommand, CancellationToken cancellationToken)
+
+        public async Task<BaseCommandResponse> Handle(CreateLeaveRequestCommand command, CancellationToken cancellationToken)
         {
             var response = new BaseCommandResponse();
             var validator = new CreateLeaveRequestDtoValidator(_leaveRequestRepository);
-            var validationResult = await validator.ValidateAsync(requestCommand.CreateLeaveRequestDto);
+            var validationResult = await validator.ValidateAsync(command.CreateLeaveRequestDto);
 
             if (validationResult.IsValid == false)
             {
@@ -37,11 +41,28 @@ namespace HR.LeaveManagement.Application.Features.LeaveRequests.Handlers.Command
                 response.Errors = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
             }
             
-            var leaveRequest = _mapper.Map<LeaveRequest>(requestCommand.CreateLeaveRequestDto);
+            var leaveRequest = _mapper.Map<LeaveRequest>(command.CreateLeaveRequestDto);
             
             response.Success = true;
             response.Message = "Creation Successful";
             response.Id = leaveRequest.Id;
+            
+            var email = new Email
+            {
+                To = "employee@org.com",
+                Subject = "Leave Request Submitted",
+                Body = $"Your leave request for {command.CreateLeaveRequestDto.StartDate:D} to {command.CreateLeaveRequestDto.EndDate:D} has been submitted successfully."
+            };
+
+            try
+            {
+                await _emailSender.SendEmailAsync(email);
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
             return response;
         }
     }
---
+++ HR.LeaveManagement.Application/Models/Email.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper.Configuration.Annotations;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.Models
+{
+    public class Email
+    {
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+    }
+}
